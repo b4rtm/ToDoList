c7d698a0f7b3e37e34e3ec6f313f6355be87da01
b4rtm <bartek.matusiak12@gmail.com>
2024-05-24 12:04:32 +0200

    add attachment class

---
+++ app/src/main/java/com/example/todolist/Attachment.kt
@@ -0,0 +1,11 @@
+package com.example.todolist
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "attachments")
+data class Attachment(
+    @PrimaryKey(autoGenerate = true) val id: Long = 0,
+    val taskId: Long,
+    val path: String,
+)
